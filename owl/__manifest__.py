71f6bd5542fb236a8fb09472517c46f0d1c54c91
odoothom <odoothom@debian12pm.com>
2024-08-03 21:30:10 -0500

    inital commit

---
+++ owl/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: uft-8 -*-
+{
+    'name': 'Owl Tutorial',
+    'version': '1.0',
+    'summary': 'OWL Tutorial - Action and Service Todo List',
+    'sequence': -1,
+    'description': """OWL Tutorial - Action and Service Todo List""",
+    'category': 'OWL',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/todo_list.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'assets': {
+        'web.assets_backend': [],
+        'web.assets_frontend': [],
+    },
+}
