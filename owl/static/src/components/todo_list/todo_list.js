b8ce36179be4df3d3424067fd54b92800402df81
odoothom <odoothom@debian12pm.com>
2024-08-14 01:05:30 -0500

    dynamic task list using ORM

--- owl/static/src/components/todo_list/todo_list.js
+++ owl/static/src/components/todo_list/todo_list.js
@@ -1,24 +1,25 @@
 /** @odoo-module **/
 
 import { registry } from '@web/core/registry';
-const { Component, useState } = owl;
+const { Component, useState, onWillStart } = owl;
+import { useService } from "@web/core/utils/hooks";
 
 export class OwlTodoList extends Component {
     setup(){
         this.state = useState({
-        taskList:[
-            {id:1, name:"Task 1", color:"#e6a200", completed: true},
-            {id:2, name:"Task 2", color:"#0c234f", completed: true},
-            {id:3, name:"Task 3", color:"#226077", completed: false},
-            {id:4, name:"Task 4", color:"#54d8a7", completed: true},
-            {id:5, name:"Task 5", color:"#addfba", completed: true},
-            {id:6, name:"Task 6", color:"#f7ecc5", completed: false},
-            {id:7, name:"Task 7", color:"#f4e4b5", completed: true},
-            {id:8, name:"Task 8", color:"#e8d9b0", completed: true},
-            {id:9, name:"Task 9", color:"#e0c8a6", completed: false},
-            {id:10, name:"Task 10", color:"#d9b288", completed: true},
-        ]
+        taskList:[]
         })
+        this.orm = useService("orm")
+        this.model = "owl.todo.list"
+
+        onWillStart(async ()=>{
+            await this.getAllTasks()
+        })
+    }
+
+    
+    async getAllTasks(){
+        this.state.taskList = await this.orm.searchRead("this.model",[],["name", "color", "completed"])
     }
 }
 
