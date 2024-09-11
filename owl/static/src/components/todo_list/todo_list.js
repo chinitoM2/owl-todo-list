252b0ecfc9fca6c37e6f595e5889953db590af33
odoothom <odoothom@debian12pm.com>
2024-09-10 19:04:30 -0500

    updated saveTask function, editTask function and addTask function

--- owl/static/src/components/todo_list/todo_list.js
+++ owl/static/src/components/todo_list/todo_list.js
@@ -7,10 +7,10 @@ import { useService } from "@web/core/utils/hooks";
 export class OwlTodoList extends Component {
     setup(){
         this.state = useState({
-        task:{name:"", color:"#FF0000", completed:false},
-        taskList:[],
-        isEdit: false,
-        activeId: false,
+            task:{name:"", color:"#FF0000", completed:false},
+            taskList:[],
+            isEdit: false,
+            activeId: false,
         })
         this.orm = useService("orm")
         this.model = "owl.todo.list"
@@ -20,22 +20,43 @@ export class OwlTodoList extends Component {
         })
     }
 
-    //helper function to automatically refresh task list data
+    // helper function to automatically refresh task list data
     async getAllTasks(){
         this.state.taskList = await this.orm.searchRead(this.model,[],["name", "color", "completed"])
     }
 
     addTask(){
-
+        this.resetForm()
+        this.state.activeId = false
+        this.state.isEdit = false
     }
 
-    editTask(){
+    editTask(task){
+        this.state.activeId = task.id
+        this.state.isEdit = true
+        this.state.task = {name: task.name, color: task.color, completed: task.completed}
     }
 
     async saveTask(){
-        console.log(this.state.task)
+        if(!this.state.isEdit){
+            await this.orm.create(this.model, [{name: this.state.task.name, color: this.state.task.color, completed: this.state.task.completed}])
+        }
+        else{
+            await this.orm.write(this.model, [this.state.activeId], this.state.task)
+        }
+
+        await this.getAllTasks()
+    }
+
+    // helper function to reset task fields when user wants to add new task
+    resetForm(){
+        this.state.task = {name:"", color:"#FF0000", completed: false}
     }
 }
 
 OwlTodoList.template = 'owl.TodoList'
 registry.category('actions').add('owl.action_todo_list_js', OwlTodoList)
+
+
+
+
