1a830f6795578265fa9897c58670ba57f88e3bf3
odoothom <thomasssdinh@gmail.com>
2024-10-09 22:16:49 -0500

    added on-click funtionalities to View Sale Orders button, View Invoices button, and View Meetings button to display quotes from Sales App, invoices from Invoicing App, and meetings from Calendar App.

--- owl/static/src/components/view_inheritance/res_partner_list_view.js
+++ owl/static/src/components/view_inheritance/res_partner_list_view.js
@@ -3,11 +3,43 @@
 import { registry } from "@web/core/registry"
 import { listView } from "@web/views/list/list_view"
 import { ListController } from "@web/views/list/list_controller"
+import { useService } from "@web/core/utils/hooks"
 
 class ResPartnerListController extends ListController {
     setup(){
         super.setup()
         console.log("this is the res partner controller for list view")
+        this.action = useService("action")
+    }
+
+    showSaleOrders(){
+        console.log("Show Sale Orders")
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Customer Sale Orders",
+            res_model: "sale.order",
+            views: [[false, "list"], [false, "form"]]
+        })
+    }
+
+    showInvoices(){
+        console.log("Show Invoices")
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Invoices",
+            res_model: "account.move",
+            views: [[false, "list"], [false, "form"]]
+        })
+    }
+
+    showMeetings(){
+        console.log("Show Meetings")
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Meetings",
+            res_model: "calendar.event",
+            views: [[false, "list"], [false, "form"]]
+        })
     }
 }
 
